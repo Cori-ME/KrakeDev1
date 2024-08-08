9903c5315ef4d722c715234a7369c3527c7e94b3
Cori-ME <joselynmosquera1992@gmail.com>
2024-08-08 18:03:19 -0500

    se separan las funciones al archivo utilitario js

---
+++ utilitarios/utilitarios.js
@@ -0,0 +1,43 @@
+mostrarTexto=function(idComponente,mensaje){
+    let componente;
+    componente=document.getElementById(idComponente);
+    componente.innerText=mensaje;
+
+}
+mostrarTextoEnCaja=function(idComponente,mensaje){
+    let componente;
+    componente=document.getElementById(idComponente);
+    componente.value=mensaje;
+
+}
+
+mostrarImagen=function(idComponente,rutaImagen){
+    let componente;
+    componente=document.getElementById(idComponente);
+    componente.src=rutaImagen;
+
+}
+
+
+
+recuperarTexto=function(idComponente){
+    let componente;
+    let valorIngresado;
+    componente=document.getElementById(idComponente);
+    valorIngresado=componente.value;
+    return valorIngresado;
+}
+recuperarInt=function(idComponente){
+    
+   let  valorCaja = recuperarTexto(idComponente);
+   let  valorEntero = parseInt(valorCaja);
+    return valorEntero;
+    
+}
+recuperarFlotante=function(idComponente){
+    
+    let valorCaja = recuperarTexto(idComponente);
+    let valorFlotante = parseFloat(valorCaja);
+    return valorFlotante;
+    
+}
