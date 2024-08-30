0df6facae9d55a6ffb4d7a1b16dad1e04bf31aac
Cori-ME <joselynmosquera1992@gmail.com>
2024-08-30 16:41:04 -0500

    tabla empleado y nuevo empleado lista

--- 1-roles/roles/roles.js
+++ 1-roles/roles/roles.js
@@ -1,12 +1,34 @@
 let empleados = [
     {cedula:"1714616123",nombre:"John",apellido:"Cena",sueldo:500.0},
-    {cedula:"0914632123",nombre:"Luisa",apellido:"Gonzalez",sueldo:900.0}
+    {cedula:"0914632123",nombre:"Luisa",apellido:"Gonzalez",sueldo:900.0},
+    {cedula:"0803997303",nombre:"Joselyn",apellido:"Mosquera",sueldo:800.0}
 ]
+mostrarEmpleado=function(){
+    let cmpTabla=document.getElementById("tablaEmpleados");
+        let contenidoTabla="<table><tr>"+
+        "<th> CEDULA</th>"+
+        "<th> NOMBRE</th>"+
+        "<th> APELLIDO</th>"+
+        "<th> SUELDO</th>"+
+        "</tr>";
+        let elementoEmpleado
+        for(let i=0;i<empleados.length;i++){
+               elementoEmpleado=empleados[i];
+               contenidoTabla+="<tr><td>"+elementoEmpleado.cedula+"</td>"+
+               "<td>"+elementoEmpleado.nombre+"</td>"
+               +"<td>"+elementoEmpleado.apellido+"</td>"
+               +"<td>"+elementoEmpleado.sueldo+"</td>"
+               +"</tr>"
+
+        }
+        contenidoTabla+="</table>"
+        cmpTabla.innerHTML=contenidoTabla;
+}
 mostrarOpcionEmpleado=function(){
     mostrarComponente("divEmpleado");
     ocultarComponente("divRol");
     ocultarComponente("divResumen");
-
+    mostrarEmpleado();
 
         
 }
